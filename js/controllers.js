deca8bcd1a663bdc8eb097756952e60fd84c993b
Darin Cassler <dcassler@unionstmedia.com>
2014-05-25 00:45:42 -0400

    prep for github page

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,14 @@
-var myNameSpace = angular.module('myApp', []);
+var myNameSpace = angular.module('ngApp', []);
 
 myNameSpace.controller('appController', function appController($scope) {
 
 	$scope.app = {
 		'title'			: 'Angular Bootstrap Quickstart',
+		'version'		: '0.1.0',
+		'progress'		: '23',
 		'description'	: 'A bare bones starting point for using Twitter Bootstrap and Angular JS for rapid application development',
 		'url'				: 'https://github.com/cassler/html5bp-twbs-angular-quickstart',
+		'downloadURL'	: 'https://github.com/cassler/html5bp-twbs-angular-quickstart/archive/master.zip',
 		'angVersion'	: '1.2.16',
 		'angUrl'			: 'https://angularjs.org/',
 		'twbsVersion'	: '3.1.1',
@@ -16,8 +19,9 @@ myNameSpace.controller('appController', function appController($scope) {
 
 	$scope.author = {
 		'name'	: 'Darin Cassler',
-		'title'	: 'Web Developer',
-		'company': 'cassler.net',
+		'title'	: 'Web Application Developer',
+		'company': 'Cassler Labs',
+		'url'		: 'http://cassler.net',
 	}
 });
 
